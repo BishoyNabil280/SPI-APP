8bc08074ba26a272659e2dec1f630242377e8a04
BishoyNabil <bishoynabil280@gmail.com>
2021-08-10 02:54:18 +0200

    SPI Application

---
+++ Master/Debug/APP/master.d
@@ -0,0 +1,43 @@
+APP/master.o APP/master.o: ../APP/master.c ../APP/../MCAL/SPI/spi.h \
+  ../APP/../MCAL/SPI/../../compiler.h \
+  ../APP/../MCAL/SPI/../../micro_config.h \
+  ../APP/../MCAL/SPI/../../common_macros.h \
+  ../APP/../MCAL/SPI/spi_registers.h ../APP/../MCAL/SPI/../DIO/DIO.h \
+  ../APP/../MCAL/SPI/../DIO/DIO_registers.h \
+  ../APP/../MCAL/SPI/../DIO/../../compiler.h \
+  ../APP/../MCAL/SPI/../DIO/../../common_macros.h \
+  ../APP/../MCAL/DIO/DIO.h ../APP/../MCAL/UART/UART.h \
+  ../APP/../MCAL/UART/../../compiler.h \
+  ../APP/../MCAL/UART/../../common_macros.h \
+  ../APP/../MCAL/UART/../../micro_config.h \
+  ../APP/../MCAL/UART/uart_registers.h
+
+../APP/../MCAL/SPI/spi.h:
+
+../APP/../MCAL/SPI/../../compiler.h:
+
+../APP/../MCAL/SPI/../../micro_config.h:
+
+../APP/../MCAL/SPI/../../common_macros.h:
+
+../APP/../MCAL/SPI/spi_registers.h:
+
+../APP/../MCAL/SPI/../DIO/DIO.h:
+
+../APP/../MCAL/SPI/../DIO/DIO_registers.h:
+
+../APP/../MCAL/SPI/../DIO/../../compiler.h:
+
+../APP/../MCAL/SPI/../DIO/../../common_macros.h:
+
+../APP/../MCAL/DIO/DIO.h:
+
+../APP/../MCAL/UART/UART.h:
+
+../APP/../MCAL/UART/../../compiler.h:
+
+../APP/../MCAL/UART/../../common_macros.h:
+
+../APP/../MCAL/UART/../../micro_config.h:
+
+../APP/../MCAL/UART/uart_registers.h:
